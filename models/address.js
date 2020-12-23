5cd8cdfadecaa791426eb7344aae1d8e637fe743
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2020-12-23 19:48:30 +0100

    Created AddressSchema

---
+++ models/address.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const AddressSchema = new Schema({
+    streetNumber: { type: String, required: true, trim: true },
+    streetName: { type: String, required: true, trim: true },
+    city: { type: String, required: true, trim: true },
+    postalCode: { type: Number, required: true, trim: true },
+    country: { type: String, required: true, trim: true },
+
+})
+
+module.exports = mongoose.model('Address', AddressSchema);
