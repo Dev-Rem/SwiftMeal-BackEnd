61501658689137c6e43e60422c7eb036d23d75b6
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2020-12-23 21:03:04 +0100

    Created ItemSchema.

---
+++ models/item.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const OItemSchema = new Schema({
+    food = { type: Schema.Types.ObjectId, ref: 'Food'},
+    discount: { type: Number, required: false},
+    quantity: { type: Number, required: false},
+},
+{
+  timestamps: true
+});
+
+module.exports = mongoose.model('Item', ItemSchema);
