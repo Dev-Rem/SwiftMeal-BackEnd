5e9bfb7ba5f8c020cfda04abd52ddb7a29fde7ad
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2021-01-13 18:11:16 +0100

    Add password hashing for account schema

--- models/account.js
+++ models/account.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 var mongooseIntlPhoneNumber = require('mongoose-intl-phone-number');
+var bycrypt = require('bcrypt'), SALT_WORK_FACTOR = 10;
 const { Schema } = mongoose;
 
 const AccountSchema = new Schema({
@@ -15,6 +16,7 @@ const AccountSchema = new Schema({
     },
     phoneNumber: { type: String, required: true, trim: true },
     address_id: { type: Schema.Types.ObjectId, ref: 'Address'},
+    passowrd: { type: String, required: true, trim: true, minlength: 8 }
 },
 {
   timestamps: true
@@ -28,4 +30,25 @@ AccountSchema.plugin(mongooseIntlPhoneNumber, {
     countryCodeField: 'countryCode',
 });
 
+AccountSchema.pre('save', (next) => {
+    var user = this;
+
+    if(!user.isModified('password')) return next();
+
+    bycrypt.genSalt(SALT_WORK_FACTOR, (error, salt) => {
+        if(error) next(error);
+        bycrypt.hash(user.password, salt, (error, hash) => {
+            if(error) next(error);
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+AccountSchema.methods.comparePassword = (candidatePassword, callback) => {
+    bycrypt.compare(candidatePassword, this.password, (error, isMatch) => {
+        if(error) return callback(error);
+        callback(undefined, isMatch);
+    });
+}
 module.exports = mongoose.model('Employee', EmployeeSchema);
---
+++ routes/account.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const Account = require('../models/account.js')
+var router = express.Router();
