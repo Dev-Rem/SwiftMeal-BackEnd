9726682bc019ab386ec872c1f3ae090ee498a5ea
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2021-01-20 10:30:41 +0100

    Add user auth middleware in account router

--- routes/accounts.js
+++ routes/accounts.js
@@ -3,6 +3,20 @@ const Account = require('../models/account.js');
 const Address = require('../models/address.js');
 const router = express.Router();
 
+
+// Create user auth
+const auth = (req, res, next) => {
+    let token = req.cookies.authToken;
+
+    Account.findByToken(token, (error, user) => {
+        if (error) throw error;
+        if(!user) res.json({isAuth: false, error: true});
+        req.token= token;
+        req.user = user;
+        next();
+    });
+}
+
 // Create a new user account
 router.post('/register', (req, res) => {
     Address.create(req.body.address, (error, address) => {
